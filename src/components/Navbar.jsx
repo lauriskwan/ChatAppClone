343a8d8257a701e3ab9ceb9310a0f72171fdb700
Lauris Kwan <kwanlauris@gmail.com>
2023-04-13 17:47:35 +0800

    navbar ui halfway

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import avatar from "../image/IMG_0285.jpg";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <span className="logo">Lama chat</span>
+      <div className="user">
+        <img src={avatar} alt="" />
+        <span>John</span>
+        <button>Logout</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
